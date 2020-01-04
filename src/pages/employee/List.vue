2e4835a6f51b7d262643377544ef0a1259833eb0
Lsy-King <2995822780@qq.com>
2020-01-04 15:11:19 +0800

    组员刘思远代码07

--- src/pages/employee/List.vue
+++ src/pages/employee/List.vue
@@ -1,22 +1,25 @@
 <template>
   <div>
-    <h>员工管理</h>
+    <h2>服务员管理页面</h2>
     <!-- 按钮 -->
     <el-button round type="success" icon="el-icon-edit" size="small" @click="toAddHandler"> 添加 </el-button>
     <el-button round type="danger" icon="el-icon-delete" size="small">批量删除</el-button>
     <!-- /按钮 -->
     <!-- 表格 -->
-    <el-table :data="employee" border style="width:90%">
+    <el-table 
+    border
+    :data="employee">
       <el-table-column type="selection" />
-      <el-table-column prop="id" label="员工编号"/>
-      <el-table-column prop="realname" label="员工姓名"/>
-      <el-table-column prop="IdCard" label="身份证"/>
-      <el-table-column prop="bankCard" label="银行卡"/>
-      <el-table-column prop="telephone" label="联系方式"/>
+      <el-table-column prop="id" label="编号" />
+      <el-table-column prop="username" label="用户名字" />
+      <el-table-column prop="realname" label="员工姓名" />
+      <el-table-column prop="type" label="工作性质" />
+      <el-table-column prop="telephone" label=" 员工联系方式" />
       <el-table-column label="操作">
         <template v-slot="slot">
-          <a href="" @click.prevent="toDeleteHandler(slot.row.id)"> <i class="el-icon-delete" /></a>
-          <a href="" @click.prevent="toUpdateHandler(slot.row)"> <i class="el-icon-edit" /></a>
+          <!-- slot 获取当前行信息 -->
+          <a href="" @click.prevent="toDeleteHandler(slot.row.id)"><i class="el-icon-delete" /></a>
+          <a href="" @click.prevent="toUpdateHandler(slot.row)"><i class="el-icon-edit" /></a>
         </template>
       </el-table-column>
     </el-table>
@@ -25,13 +28,18 @@
     <el-pagination layout="prev, pager, next" :total="50" />
     <!-- /分页结束 -->
     <!-- 模态框 -->
-    <el-dialog title="录入员工信息" :visible.sync="visible" width="40%">
-      <el-form :model="form" label-width="90px">
-        <el-form-item label="员工姓名">  <el-input v-model="form.username"/>  </el-form-item>
-        <el-form-item label="员工身份证"> <el-input v-model="form.IdCard"/>   </el-form-item>
-        <el-form-item label="员工银行卡"> <el-input v-model="form.backCard"/> </el-form-item>
-        <el-form-item label="联系方式">  <el-input v-model="form.telephone"/> </el-form-item>
-      </el-form>
+    <el-dialog
+      title="录入地址信息"
+      :visible.sync="visible"
+      width="50%"
+    >
+      <el-form :model="form" label-width="100px">
+        <el-form-item label="编号" width="100px"><el-input v-model="form.id" /></el-form-item>
+        <el-form-item label="用户姓名" width="100px"><el-input v-model="form.username" /></el-form-item>
+        <el-form-item label="员工姓名" width="100px"><el-input v-model="form.realname" /> </el-form-item>
+        <el-form-item label="工作性质" width="100px"><el-input v-model="form.type" /> </el-form-item>
+        <el-form-item label="员工联系方式" width="100px"><el-input v-model="form.telephone" /> </el-form-item>
+          </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button round size="small" @click="closeModalHandler">取 消</el-button>
         <el-button round size="small" type="primary" @click="submitHandler">确 定</el-button>
@@ -45,6 +53,7 @@
 <script>
 import request from '@/utils/request'
 import querystring from 'querystring'
+// import { type } from 'os'
 export default
 {
   // 用于存放要向网页中显示的数据
@@ -53,7 +62,7 @@ export default
       visible: false,
       employee: [],
       form: {
-        type: 'waiter'
+        type: 'employee'
       }
     }
   },
@@ -72,7 +81,7 @@ export default
       })
     },
     submitHandler() {
-      // this.form对象---字符串--》后台
+      // this.form对象---字符串-->后台
       // 通过request与后台进行交互,需携带参数
       const url = 'http://localhost:6677/waiter/saveOrUpdate'
       request({
@@ -101,15 +110,14 @@ export default
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        // 调用后台接口，完成删除信息
+        // 调用后台接口，完成删除
         const url = 'http://localhost:6677/waiter/deleteById?id=' + id
         request.get(url).then((response) => {
           // 刷新数据
           this.loadData()
-          // 提示结果
           this.$message({
             type: 'success',
-            message: response.message
+            message: '删除成功!'
           })
         })
       })
@@ -121,7 +129,6 @@ export default
     closeModalHandler() {
       this.visible = false
     },
-
     toAddHandler() {
       this.form = {
         type: 'employee'
